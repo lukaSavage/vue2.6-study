e44c8d9e5d422ba5e222757801445b15d34b556b
lukaSavage <tu14181@qq.com>
2022-01-23 18:16:56 +0800

    feat: ast语法树的生成

--- src/init.js
+++ src/init.js
@@ -3,9 +3,10 @@
  * @Author: lukasavage
  * @Date: 2022-01-19 21:48:36
  * @LastEditors: lukasavage
- * @LastEditTime: 2022-01-20 09:38:53
+ * @LastEditTime: 2022-01-23 13:30:39
  */
 
+import { compileToFunctions } from './compiler';
 import { initState } from './state';
 
 export function initMixin(Vue) {
@@ -15,5 +16,22 @@ export function initMixin(Vue) {
 		vm.$options = options;
 		// note: 2、初始化状态(将数据做一个初始化的劫持，当数据改变时更新视图)
 		initState(vm);
+	};
+
+	Vue.prototype.$mount = function (el) {
+		// 挂载操作
+		const vm = this;
+		const options = vm.$options;
+		el = document.querySelector('#app');
+		// 优先级判断 render > template > el
+		if (!options.render) {
+			// 没render方法，将template转换成render方法
+			let template = options.template;
+			if (!template && el) {
+				template = el.outerHTML;
+			}
+			const render = compileToFunctions(template);
+            options.render = render;
+		}
 	};
 }
