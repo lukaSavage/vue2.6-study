a839065fade887588913cb14d30388506da9862a
lukaSavage <tu14181@qq.com>
2022-01-25 00:16:26 +0800

    feat: 实现render函数生成

---
+++ src/compiler/generate.js
@@ -0,0 +1,90 @@
+/*
+ * @Descripttion: 通过ast语法树生成render函数
+ * @Author: lukasavage
+ * @Date: 2022-01-24 20:40:17
+ * @LastEditors: lukasavage
+ * @LastEditTime: 2022-01-25 00:14:52
+ */
+
+const defaultTagRE = /\{\{((?:.|\r?\n)+?)\}\}/g; // 匹配双花括号语法 {{ xxx }}
+
+export function generate(ast) { 
+	/* 
+        note: 通过ast语法树生成如下结构↓↓↓
+        <div id="my">hello,<span>{{ str }}</span></div>
+
+        render() {
+            return _c('div', {id: 'my'}, _v('hello,'),_c('span', undefined, _s('s')))
+        }
+
+    */
+	function gen(node) {
+		if (node.type === 1) {
+			// 说明是元素
+			return generate(node);
+		} else {
+			let txt = node.text;
+            console.log(txt);
+			if (!defaultTagRE.test(txt)) {
+				// 如果是普通文本(不带双花括号)
+				return `_v(${JSON.stringify(txt)})`;
+				// 处理_v("{{str}}")、_v("呵呵 {{str}}"),将其转换成_v(_s(str))、_v('呵呵 ' + _s(str))
+			}
+			let tokens = []; // 用此数组存放每一段上面的代码
+			let lastIndex = (defaultTagRE.lastIndex = 0); // 如果正则是全局模式，默认test指针会指向1，所以需要将lastIndex置为0
+			let match, index; // 每次匹配的结果
+			while ((match = defaultTagRE.exec(txt))) {
+                index = match.index; // 保存匹配到的索引
+				if (index > lastIndex) {
+					tokens.push(JSON.stringify(txt.slice(lastIndex, index)));
+				}
+				tokens.push(`_s(${match[1].trim()})`);
+				lastIndex = index + match[0].length;
+			}
+			// '}}' 后面的内容处理
+			if (lastIndex < txt.length) {
+				tokens.push(JSON.stringify(txt.slice(lastIndex)));
+			}
+            console.log(tokens);
+			return `_v(${tokens.join('+')})`;
+		}
+	}
+	function genChildren(el) {
+		const children = el.children;
+		if (children) {
+			// 将所有转化后的儿子用逗号拼接起来
+			return children.map(item => gen(item)).join(',');
+		}
+	}
+
+	let children = genChildren(ast);
+	let renderChild = children ? `,${children}` : '';
+	let code = `_c(${ast.tag},${
+		ast.attrs.length ? String(genProps(ast.attrs)) : 'undefined'
+	}${renderChild})`;
+
+	console.log(code);
+	return code;
+}
+
+function genProps(attrs) {
+	let str = '';
+	for (let i = 0; i < attrs.length; i++) {
+		const values = attrs[i];
+        console.log(values);
+		if (values.name === 'style') {
+			let obj = {}; // 对样式做处理
+			values.value.split(';').forEach(item => {
+				let [key, value] = item.split(':');
+				obj[key] = value;
+			});
+			values.value = obj;
+		}
+		str += `${values.name}:${JSON.stringify(values.value)},`;
+	}
+
+	return `{${str.slice(0, -1)}}`;
+}
+
+
+
--- src/compiler/index.js
+++ src/compiler/index.js
@@ -3,19 +3,11 @@
  * @Author: lukasavage
  * @Date: 2022-01-23 13:29:14
  * @LastEditors: lukasavage
- * @LastEditTime: 2022-01-23 18:14:31
+ * @LastEditTime: 2022-01-25 00:14:38
  */
 
-// todo: 这里我们以 <div id="my">hello,<span>{{ str }}</span></div> 为例来解析
-
-const ncname = `[a-zA-Z_][\\-\\.0-9_a-zA-Z]*`; // 匹配标签名
-const qnameCapture = `((?:${ncname}\\:)?${ncname})`; // 匹配类似 <my:xx></my:xx> 的标签
-const startTagOpen = new RegExp(`^<${qnameCapture}`); // 标签开头的正则 捕获的内容是标签名
-const endTag = new RegExp(`^<\\/${qnameCapture}[^>]*>`); // 匹配标签结尾的 </div>
-const attribute =
-	/^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/; // 匹配属性的，例如 a='xxx' a=xxx  a="xxx"
-const startTagClose = /^\s*(\/?)>/; // 匹配标签结束的 >
-const defaultTagRE = /\{\{((?:.|\r?\n)+?)\}\}/g; // 匹配双花括号语法 {{ xxx }}
+import { generate } from './generate';
+import { parseHTML } from './parse';
 
 export function compileToFunctions(template) {
 	// todo: 将template编译成html
@@ -23,123 +15,14 @@ export function compileToFunctions(template) {
         具体做法：
             1、将html模板转换成ast语法书
             2、通过ast树重新生成代码
+            3、todo: 优化静态节点
+            4、通过树生成代码
     */
 	const ast = parseHTML(template);
-	console.log(ast);
-}
-
-function parseHTML(html) {
-	let root;
-	let currentParent; // 用来记录当前的父节点
-	let stacks = []; // 用于检测标签是否合法，例如<div><span></div></span>
-	while (html) {
-		// tag: 只要html为空，就一直解析
-		const tag = html.indexOf('<');
-		if (tag === 0) {
-			// 说明肯定是标签,开始解析开始标签
-			const startTagMatch = parseStartTag();
-			if (startTagMatch) {
-				start(startTagMatch.name, startTagMatch.attrs);
-				continue;
-			}
-			// 处理结束标签
-			const endTagMatch = html.match(endTag);
-			if (endTagMatch) {
-				advance(endTagMatch[0].length);
-				// 将结束标签传入
-				end(endTagMatch[1]);
-				continue;
-			}
-		}
-		let txt;
-		if (tag > 0) {
-			// 表示文本,先用txt存起来，再删除掉
-			txt = html.slice(0, tag);
-		}
-		if (txt) {
-			advance(txt.length);
-			chars(txt);
-		}
-	}
-	/**
-	 * 截取字符串,之后更新html中的内容
-	 * @param {number} n 从索引几开始
-	 */
-	function advance(n) {
-		html = html.slice(n);
-	}
-	function parseStartTag() {
-		const startTag = html.match(startTagOpen);
-		if (startTag) {
-			const match = {
-				tagName: startTag[1],
-				attrs: [],
-			};
-			advance(startTag[0].length); // 删除开始标签  id="my">hello,<span>{{ str }}</span></div>
-			// note: 2、开始处理标签的属性
-			// 如果直接是闭合标签，说明没有属性
-			let end, attr;
-			// 不是结尾标签，并且能匹配到属性
-			while (
-				!(end = html.match(startTagClose)) &&
-				(attr = html.match(attribute))
-			) {
-				match.attrs.push({
-					name: attr[1],
-					value: attr[3] || attr[4] || attr[5],
-				});
-				advance(attr[0].length); // 去掉当前属性
-			}
-			if (end) {
-				// 开始标签的 >
-				advance(end[0].length);
-				return match;
-			}
-		}
-	}
-	/* 解析开始标签 */
-	function start(tagName, attrs) {
-		// console.log('收到start标签了:', tagName, attrs);
-		let element = createASTElement(tagName, attrs);
-		if (!root) {
-			root = element;
-		}
-		// 记录当前的父元素的ast
-		currentParent = element;
-		stacks.push(element);
-	}
-
-	/* 解析结束标签 */
-	function end(tagName) {
-		let element = stacks.pop();
-		currentParent = stacks[stacks.length - 1]; // 去除栈中的最后一个
-		if (currentParent) {
-			element.parent = currentParent;
-			currentParent.children.push(element);
-		}
-	}
-
-	/* 解析标签的文本 */
-	function chars(text) {
-		// console.log('收到chars标签了:', text);
-		text = text.replace(/\s/g, '');
-		if (text) {
-			currentParent.children.push({
-				type: 3,
-				text,
-			});
-		}
-	}
-
-	function createASTElement(tagName, attrs) {
-		return {
-			tag: tagName, // 标签名
-			type: 1, // 元素类型
-			children: [], // 孩子列表
-			attrs, // 属性集合
-			parent: null, // 父元素
-		};
-	}
-    console.log(root);
-	return root;
+	// todo: 优化静态节点
+    
+	// 4.通过树生成代码
+	const code = generate(ast);
+	let render = new Function(`with(this){return ${code}}`);
+    return render;
 }
