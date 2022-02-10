d922019684adf60a1b601a4b5ebccb0c562eaf84
tuyx01 <tuyx01@mingyuanyun.com>
2022-02-10 17:45:46 +0800

    feat: dep类和watcher类相互绑定

--- src/observe/dep.js
+++ src/observe/dep.js
@@ -3,17 +3,27 @@
  * @Author: lukasavage
  * @Date: 2022-01-28 15:20:55
  * @LastEditors: lukasavage
- * @LastEditTime: 2022-01-28 15:43:32
+ * @LastEditTime: 2022-02-10 17:39:57
  * @FilePath: \vue-demo\src\observe\dep.js
  */
 
+let id = 0; // 用于给dep添加一个唯一标识，以便去重
 class Dep {
     subs = [];
+    id = id++;
     depend() {
-        this.subs.push(Dep.target);
+        /* 
+            为了后续实现computed，我们希望Watcher可以存放dep
+            让watcher记住dep的同时，也让dep记住watcher
+        */
+        Dep.target.addDep(this);
+        // this.subs.push(Dep.target);
+    }
+    addSubs(watcher) {
+        this.subs.push(watcher);
     }
     notify() {
-        this.subs.forEach(watcher=>watcher.update())
+        this.subs.forEach(watcher => watcher.update());
     }
 }
 
--- src/observe/watcher.js
+++ src/observe/watcher.js
@@ -3,11 +3,10 @@
  * @Author: lukasavage
  * @Date: 2022-01-28 14:53:48
  * @LastEditors: lukasavage
- * @LastEditTime: 2022-02-10 10:21:52
+ * @LastEditTime: 2022-02-10 17:32:48
  * @FilePath: \vue-demo\src\observe\watcher.js
  */
 import { popTarget, pushTarget } from './dep';
-
 
 let id = 0;
 class Watcher {
@@ -17,11 +16,22 @@ class Watcher {
         this.cb = cb;
         this.options = options;
         this.id = id++; // watcher的唯一标识
+        this.deps = []; // 盛放dep的容器
+        this.depsId = new Set();
 
         if (typeof exprOrFn === 'function') {
             this.getters = exprOrFn;
         }
         this.get(); // 默认会调用get方法
+    }
+
+    addDep(dep) {
+        let id = dep.id;
+        if (!this.depsId.has(id)) {
+            this.deps.push(dep);
+            this.depsId.add(id);
+            dep.addSubs(this);
+        }
     }
 
     get() {
